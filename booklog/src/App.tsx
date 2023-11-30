5dc34a1eb413fefc035718e1c9bc6ce2925a972e
emu <j.emura@newbees.jp>
2023-11-30 18:03:52 +0900

    Step2の反映

--- booklog/src/App.tsx
+++ booklog/src/App.tsx
@@ -5,41 +5,51 @@ import './App.css'
 import './booklist.css'
 
 function App() {
-  const [item, setItem] = useState<BookItem>(MOCK_DATA.items[0])
-  // モックデータのitemsをuseStateで管理
-  // 現時点ではuseStateを使っていないのでsetItemが赤字になる？→setItemは省略できる！
+  const [items, setItems] = useState<BookItem[]>(MOCK_DATA.items)
+  //複数のデータを格納(念の為複数形に変更)。BookItemの型も配列を指定。
+
   return (
     <>
-      <div className="u-container">
-        <ul className="c-book-list">
-          <li className="c-book-list__item">
-            {item.volumeInfo.imageLinks && ( //論理演算子でimageLinksがある時のみ表示。保存時に"()"がつく時とつかない時の差は？？
-              <div className="c-book-list__img">
-                <img src={item.volumeInfo.imageLinks.thumbnail} alt={item.volumeInfo.title} />
-              </div>
-            )}
-            <div className="c-book-list__text">
-              <h2 className="c-book-list__title">{item.volumeInfo.title}</h2>
-              {item.volumeInfo.description && <p className="c-book-list__description">{item.volumeInfo.description}</p>}
-              <div className="c-book-list__other">
-                {item.volumeInfo.authors && (
-                  <div className="c-book-list__authors">著者：{item.volumeInfo.authors.join('、')}</div>
+      <header className="l-header">
+        <h1 className="l-header__logo">Booklog</h1>
+      </header>
+      <main className="l-main">
+        <div className="u-container">
+          <ul className="c-book-list">
+            {items.map((item) => (
+              //格納したデータをループ表示
+              <li className="c-book-list__item">
+                {item.volumeInfo.imageLinks && (
+                  <div className="c-book-list__img">
+                    <img src={item.volumeInfo.imageLinks.thumbnail} alt={item.volumeInfo.title} />
+                  </div>
                 )}
-                {item.volumeInfo.publisher && (
-                  <div className="c-book-list__publisher">出版社：{item.volumeInfo.publisher}</div>
-                )}
-              </div>
-              {item.volumeInfo.previewLink && (
-                <div className="c-book-list-action">
-                  <a className="c-book-list-action__link" href={item.volumeInfo.previewLink} target="_blank">
-                    詳しく見る
-                  </a>
+                <div className="c-book-list__text">
+                  <h2 className="c-book-list__title">{item.volumeInfo.title}</h2>
+                  {item.volumeInfo.description && (
+                    <p className="c-book-list__description">{item.volumeInfo.description}</p>
+                  )}
+                  <div className="c-book-list__other">
+                    {item.volumeInfo.authors && (
+                      <div className="c-book-list__authors">著者：{item.volumeInfo.authors.join('、')}</div>
+                    )}
+                    {item.volumeInfo.publisher && (
+                      <div className="c-book-list__publisher">出版社：{item.volumeInfo.publisher}</div>
+                    )}
+                  </div>
+                  {item.volumeInfo.previewLink && (
+                    <div className="c-book-list-action">
+                      <a className="c-book-list-action__link" href={item.volumeInfo.previewLink} target="_blank">
+                        詳しく見る
+                      </a>
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          </li>
-        </ul>
-      </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
     </>
   )
 }
