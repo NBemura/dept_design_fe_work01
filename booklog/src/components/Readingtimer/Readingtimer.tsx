bf96c15c7634ff744d0e2ef0b1e8b5024fb7686c
emu <j.emura@newbees.jp>
2024-01-23 19:02:55 +0900

    Step4 selectの管理方法の修正、型の実装

--- booklog/src/components/Readingtimer/Readingtimer.tsx
+++ booklog/src/components/Readingtimer/Readingtimer.tsx
@@ -1,63 +1,54 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './readingtimer.css'
 
 export function Readingtimer() {
-  //selectの時間を管理
-  const [selectedTime, setSelectedTime] = useState(5)
+  //useRefでselectを参照
+  const selectedTimeRef = useRef<HTMLSelectElement>(null)
   //タイマーの残り時間を管理
-  const [currentTime, setCurrentTime] = useState(0)
+  const [currentTime, setCurrentTime] = useState<number>(0)
   //タイマーが実行中かどうかを管理
-  const [timerRunning, setTimerRunning] = useState(false)
+  const [timerRunning, setTimerRunning] = useState<boolean>(false)
 
   console.count('レンダリングです')
 
-  //１）selectのvalueが変わるとselectedTimeを更新
-  const handleTimeChange = (e) => {
-    //parseIntで文字列を整数化
-    setSelectedTime(parseInt(e.target.value, 10))
-    //この時点では更新前のselectedTimeが表示される可能性
-    console.log(selectedTime)
-  }
-
-  //２）スタートボタン
-  const handleStartSubmit = (e) => {
+  //スタートボタン
+  const handleStartSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     //実行中をtrueにする
     setTimerRunning(true)
-    //残り時間をcurrentTimeにセット
-    setCurrentTime(selectedTime)
-    console.log('start!' + selectedTime)
+    //残り時間をcurrentTimeにセット。
+    //refオブジェクトは.currentでDOMにアクセスでき、文字列で取得されるためNumberで数値化
+    //（parseIntからNumberにするとエラーが消えたのですが、使い分けがよくわかっていません…）
+    setCurrentTime(Number(selectedTimeRef.current?.value))
   }
 
   //第二引数[timerRunning]が更新されたら実行
   useEffect(() => {
     console.count('useEffectです')
 
-    if (timerRunning) {
-      const intervalId = setInterval(() => {
-        setCurrentTime((num) => {
-          if (num > 0) {
-            return num - 1
-          } else {
-            clearInterval(intervalId)
-            setTimerRunning(false)
-            return 0
-          }
-        })
-      }, 1000)
-      //初期化関数（無限ループ回避）
-      return () => {
-        clearInterval(intervalId)
-      }
+    if (!timerRunning) return
+    const intervalId = setInterval(() => {
+      setCurrentTime((num: number) => {
+        if (num > 0) {
+          return num - 1
+        } else {
+          clearInterval(intervalId)
+          setTimerRunning(false)
+          return 0
+        }
+      })
+    }, 1000)
+    //初期化関数（無限ループ回避）
+    return () => {
+      clearInterval(intervalId)
     }
   }, [timerRunning])
 
   //タイマーの整形
-  const formatTime = (time) => {
-    const hours = Math.floor(time / 3600)
-    const minutes = Math.floor((time / 60) % 60)
-    const seconds = time % 60
-    // return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+  const formatTime = (time: number) => {
+    const hours: number = Math.floor(time / 3600)
+    const minutes: number = Math.floor((time / 60) % 60)
+    const seconds: number = time % 60
     return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
   }
 
@@ -66,10 +57,11 @@ export function Readingtimer() {
       <h2 className="c-reading-timer__title">読書タイマー</h2>
       <div className="c-reading-timer__counter">{formatTime(currentTime)}</div>
       <form className="c-reading-timer__form" onSubmit={handleStartSubmit}>
-        <select className="c-reading-timer__select" value={selectedTime} onChange={handleTimeChange}>
-          <option value={5}>5秒</option>
-          <option value={10}>10秒</option>
-          <option value={15}>15秒</option>
+        {/* useRefを使うことで、変更のたびにレンダリングを発生しないようにできる。（非制御コンポーネント） */}
+        <select className="c-reading-timer__select" name="slectedTime" ref={selectedTimeRef}>
+          <option value="5">5秒</option>
+          <option value="10">10秒</option>
+          <option value="15">15秒</option>
         </select>
         <button className="c-reading-timer__button">Start</button>
       </form>
